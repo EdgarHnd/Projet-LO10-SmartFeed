521713e46a21f248f7ee14bc66a09ae99b12b8c5
Guillaume Segura <gui.segura78@gmail.com>
2021-06-09 13:49:21 +0200

    installation node

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fetch = require('node-fetch');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -9,3 +10,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet%2Cid&channelId=UCjWRi2qaGtKjQyoQLc4OGkw&maxResults=3&order=date&key=AIzaSyACECAgOBGMjiQ46_Sp2UnJMim0s878LCw')
+  .then(res => res.json())
+  .then(json => console.log(json));
