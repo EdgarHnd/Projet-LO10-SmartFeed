1998131e2997032b3911e877a12e4ea0f0d48365
Edgar Haond <edgar.haond@gmail.com>
2021-06-11 17:04:31 +0200

    added twitter.js

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ app.get('/', (req, res) => {
 app.get("/reddit/posts", (req, res) => {
     //méthod async afin d'utiliser await pour le fetch
     (async() => {
-        try{
-            my_json = {media : "reddit", subscribe: []};
+        try {
+            my_json = { media: "reddit", subscribe: [] };
             let subs = req.query.sub;
-            arr_subs = subs.split(',');
+            arr_suzbs = subs.split(',');
 
-            for (let sub of arr_subs){
+            for (let sub of arr_subs) {
                 var posts = [];
 
                 var request = await fetch('https://www.reddit.com/r/' + sub + '/hot.json').then(response => response.json());
                 var sub_name = request["data"]["children"][0]["data"]["subreddit_name_prefixed"];
 
-                for(let post of request["data"]["children"]){
+                for (let post of request["data"]["children"]) {
                     let content = post["data"]["title"];
                     let url = post["data"]["url"];
                     let author = "u/".concat(post["data"]["author"]);
                     let thumbnail = post["data"]["thumbnail"];
-                    let my_post = {content: content, url: url, author: author, thumbnail: thumbnail};
+                    let my_post = { content: content, url: url, author: author, thumbnail: thumbnail };
 
                     posts.push(my_post);
                 }
-                my_json["subscribe"].push({name: sub_name, posts: posts})
+                my_json["subscribe"].push({ name: sub_name, posts: posts })
             }
             res.send(my_json);
-        } catch(error) {
+        } catch (error) {
             console.log(error);
         }
-        
+
     })();
 })
 
